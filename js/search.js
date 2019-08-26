7f7753b7e89a7cd383c119bbe82c68298b4f554b
ci-build <antoniogamiz@gmail.com>
2019-08-26 07:21:33 +0000

    [skip ci] Updates

--- js/search.js
+++ js/search.js
@@ -143,15 +143,7 @@ $(function(){
 ,
 { category: "class", value: "Perl6::Documentable::DocPage::Index::Programs", url: "/Perl6-Documentable/type/Perl6::Documentable::DocPage::Index::Programs" }
 ,
-{ category: "class", value: "Perl6::Documentable::DocPage::Factory", url: "/Perl6-Documentable/type/Perl6::Documentable::DocPage::Factory" }
-,
-{ category: "class", value: "Perl6::Documentable::Index", url: "/Perl6-Documentable/type/Perl6::Documentable::Index" }
-,
 { category: "class", value: "Perl6::Documentable::Search", url: "/Perl6-Documentable/type/Perl6::Documentable::Search" }
-,
-{ category: "class", value: "Perl6::Documentable::Secondary", url: "/Perl6-Documentable/type/Perl6::Documentable::Secondary" }
-,
-{ category: "class", value: "Perl6::Documentable::Registry", url: "/Perl6-Documentable/type/Perl6::Documentable::Registry" }
 ,
 { category: "class", value: "Perl6::Documentable::Primary", url: "/Perl6-Documentable/type/Perl6::Documentable::Primary" }
 ,
@@ -161,19 +153,27 @@ $(function(){
 ,
 { category: "class", value: "X::Documentable::MissingMetadata", url: "/Perl6-Documentable/type/X::Documentable::MissingMetadata" }
 ,
+{ category: "class", value: "Perl6::Documentable::Index", url: "/Perl6-Documentable/type/Perl6::Documentable::Index" }
+,
+{ category: "class", value: "Perl6::Documentable::Secondary", url: "/Perl6-Documentable/type/Perl6::Documentable::Secondary" }
+,
+{ category: "class", value: "Perl6::Documentable::Registry", url: "/Perl6-Documentable/type/Perl6::Documentable::Registry" }
+,
 { category: "class", value: "Perl6::Documentable::Config", url: "/Perl6-Documentable/type/Perl6::Documentable::Config" }
 ,
 { category: "class", value: "X::Documentable::Config::InvalidConfig", url: "/Perl6-Documentable/type/X::Documentable::Config::InvalidConfig" }
+,
+{ category: "class", value: "Perl6::Documentable::DocPage::Factory", url: "/Perl6-Documentable/type/Perl6::Documentable::DocPage::Factory" }
 ,
 { category: "grammar", value: "Perl6::Documentable::Heading::Grammar", url: "/Perl6-Documentable/type/Perl6::Documentable::Heading::Grammar" }
 ,
 { category: "class", value: "Perl6::Documentable::To::HTML::Wrapper", url: "/Perl6-Documentable/type/Perl6::Documentable::To::HTML::Wrapper" }
 ,
-{ category: "Language", value: "Pod6 Format", url: "/Perl6-Documentable/language/document-format" }
-,
 { category: "Language", value: "Command line interface", url: "/Perl6-Documentable/language/cli" }
 ,
 { category: "Language", value: "Configuration file", url: "/Perl6-Documentable/language/configuration-file" }
+,
+{ category: "Language", value: "Pod6 Format", url: "/Perl6-Documentable/language/document-format" }
 ,
 { category: "Language", value: "TypeGraph representations", url: "/Perl6-Documentable/language/typegraph" }
 ,
